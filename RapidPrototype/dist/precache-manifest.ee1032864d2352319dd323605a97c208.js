5a2d5714c5370d3d8bd91eae46d3d14b7d3f5a45
Joel Mai <38419872+Inf166@users.noreply.github.com>
2020-12-29 19:51:21 +0100

    Trying to fix deployment

---
+++ RapidPrototype/dist/precache-manifest.ee1032864d2352319dd323605a97c208.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a8bc7be42fb1cb341b",
+    "url": "/css/app.3a8bf70a.css"
+  },
+  {
+    "revision": "163fed3f686c5ddfb4a834239daf3e6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03154811eae317c91831",
+    "url": "/js/about.a1dbce59.js"
+  },
+  {
+    "revision": "39a8bc7be42fb1cb341b",
+    "url": "/js/app.eeef2913.js"
+  },
+  {
+    "revision": "c97f45c06424bd84639f",
+    "url": "/js/chunk-2d0d6f50.f6a96a91.js"
+  },
+  {
+    "revision": "27ed5783830fbe816dd3",
+    "url": "/js/chunk-2d213164.035c0bdd.js"
+  },
+  {
+    "revision": "3d69a576331e89322deb",
+    "url": "/js/chunk-2d217357.caa7161e.js"
+  },
+  {
+    "revision": "a3f5937b9a2dc1491807",
+    "url": "/js/chunk-vendors.8b7018b9.js"
+  },
+  {
+    "revision": "bf6bd0504d9d2db3a62f6e9fd3caf270",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
