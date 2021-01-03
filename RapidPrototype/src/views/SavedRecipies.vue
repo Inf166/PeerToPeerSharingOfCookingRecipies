c6215a0923573f3903bdf76f512ca41bc8234a1b
Derya Coban <deryacbn@gmx.de>
2021-01-03 15:03:37 +0100

    Update SavedRecipies.vue

--- RapidPrototype/src/views/SavedRecipies.vue
+++ RapidPrototype/src/views/SavedRecipies.vue
@@ -1,5 +1,28 @@
-<template>
-  <div class="savedrecipies">
-    <h1>This is an SAVEDRECIPIES page</h1>
-  </div>
-</template>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+img {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  width: 150px;
+}
+
+img:hover {
+  box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+}
+</style>
+</head>
+<body>
+
+<h2>Gespeicherte Rezepte</h2>
+<p>Nudelauflauf</p>
+
+<a target="_blank" href="spagetti.jpg">
+  <img src="spagetti.jpg" alt="Forest" style="width:150px">
+</a>
+
+</body>
+</html>
