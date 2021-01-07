bb24acf9c0dda207c8286b52436012faae0a819c
Joel Mai <38419872+Inf166@users.noreply.github.com>
2021-01-07 19:28:25 +0100

    removed nav-label because mobile was broken

--- RapidPrototype/src/components/UserLoginState.vue
+++ RapidPrototype/src/components/UserLoginState.vue
@@ -3,18 +3,18 @@
     <div id="newuser">
       <router-link to="/login" id="login-button">
         <i class="fas fa-sign-in-alt"></i>
-        <span class="nav-label">Anmelden</span>
+        <span class="">Anmelden</span>
       </router-link>
       oder
       <router-link to="/signin" id="signin-button">
         <i class="fas fa-sign-in-alt"></i>
-        <span class="nav-label">Registrieren</span>
+        <span class="">Registrieren</span>
       </router-link>
     </div>
     <div id="currentuser">
       <router-link to="/logout" id="logout-button">
         <i class="fas fa-sign-out-alt"></i>
-        <span class="nav-label">Abmelden</span>
+        <span class="">Abmelden</span>
       </router-link>
     </div> 
   </div>
