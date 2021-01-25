11a1a2891eba99f9b39401868f6fc4d04c61b8fc
Joel Mai <38419872+Inf166@users.noreply.github.com>
2021-01-25 20:35:17 +0100

    deployed

---
+++ RapidPrototype/Frontend/dist/js/index.js
@@ -0,0 +1,145 @@
+let userloggedin = false;
+
+window.addEventListener('load', () => {
+  if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('../firebase-messaging-sw.js').then(registration => {
+          console.log('SW registered: ', registration);
+      }).catch(registrationError => {
+          console.log('SW registration failed: ', registrationError);
+      });
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("email_field_login").value;
+  var userPass = document.getElementById("password_field_login").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+  .then((user) => {
+    window.location = "../"
+    if (user != null) {
+      email = user.email;
+      photoUrl = user.photoURL;
+      emailVerified = user.emailVerified;
+      uid = user.uid;  
+      console.log("Hello " + email);
+      userloggedin = true;
+    }
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    window.alert("Error : " + errorMessage);
+    userloggedin = false;
+    // ...
+  });
+
+}
+
+function signin(){
+
+  var userEmail = document.getElementById("email_field_signin").value;
+  // var userName = document.getElementById("name_field_signin").value;
+  var userPass = document.getElementById("password_field_signin").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass)
+  .then(() => {
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    .then((user) => {
+      userloggedin = true;
+      window.location = "../"
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      window.alert("Error : " + errorMessage);
+      // ...
+    });
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    window.alert("Error : " + errorMessage);
+    // ..
+  });
+// [END auth_signup_password]
+
+}
+
+function logout(){
+  firebase.auth().signOut().then(() => {
+    window.location = "../"
+    userloggedin = false;
+  });
+}
+
+window.onload = () => {
+  console.log("Window Loaded");
+  checkUserLoginState()
+}
+
+function checkUserLoginState() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      console.log("A User was found");
+      document.getElementById("currentuser").style.display = "block";
+      document.getElementById("newuser").style.display = "none";
+  
+      var user = firebase.auth().currentUser;
+  
+      if(userloggedin){
+        email = user.email;
+        photoUrl = user.photoURL;
+        emailVerified = user.emailVerified;
+        uid = user.uid;  
+        document.getElementById("greetUser").innerHTML = "Hallo " + email + "!";
+        console.log("Hello and Welcome " + email);
+        userloggedin = true;
+        // document.getElementById("userprofileimage").setAttribute("src", photoUrl);
+        // console.log("UserProfile " + photoUrl);
+      }
+  
+    } else {
+      // No user is signed in.
+      console.log("No user is signed in");
+      document.getElementById("currentuser").style.display = "none";
+      document.getElementById("newuser").style.display = "block";
+      userloggedin = false;
+    }
+  });
+
+  var peer = new Peer(); 
+  var conn = peer.connect('another-peers-id');
+  // on open will be launch when you successfully connect to PeerServer
+  conn.on('open', function(){
+    // here you have conn.id
+    console.log("sending Data");
+    conn.send('hi!');
+  });
+  peer.on('connection', function(conn) {
+    conn.on('data', function(data){
+      // Will print 'hi!'
+      console.log("receiving Data");
+      console.log(data);
+    });
+  });
+  // peer.on('open', function() {
+  //     console.log('My peerjsPeer ID is: ', peerjsPeer.id);
+  //   });
+  
+  // peer.on('connection', function(peerjsConnection) {
+  //   peerjsConnection.on('open', function() {
+  //     // Receive messages
+  //     peerjsConnection.on('data', function(data) {
+  //       console.log('Received', data);
+  //     });
+  //     // Send messages
+  //     peerjsConnection.send('Hello from markers-page!');
+  //   });
+  // });
+}
---
+++ RapidPrototype/Frontend/dist/precache-manifest.50319c29b1c7aaeae70d96dab09d0e8b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5f8881da4c3b696fa4",
+    "url": "/css/app.9b87f532.css"
+  },
+  {
+    "revision": "7d9914c6a30b23f8f72f",
+    "url": "/css/chunk-40fa87f4.bf415ed1.css"
+  },
+  {
+    "revision": "fbc1f657b2a55750c60e1e8693ebee86",
+    "url": "/data/cached/recipies/recipe-template.json"
+  },
+  {
+    "revision": "b6783ce1a6de8ce267990b3232cbf124",
+    "url": "/firebase-messaging-sw.js"
+  },
+  {
+    "revision": "cbad573a5dd3cd87cfe079848b68a182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7e4023e390b990d1a01",
+    "url": "/js/about.e4110cc5.js"
+  },
+  {
+    "revision": "cf5f8881da4c3b696fa4",
+    "url": "/js/app.25bdc096.js"
+  },
+  {
+    "revision": "8259bab51e2c8b50076b",
+    "url": "/js/chunk-2d0aedb5.858f1554.js"
+  },
+  {
+    "revision": "f03f212e971a98dd4d24",
+    "url": "/js/chunk-2d0c2117.59f45a13.js"
+  },
+  {
+    "revision": "9fe98ae20d3a3ddd1707",
+    "url": "/js/chunk-2d0c8842.44af2dc5.js"
+  },
+  {
+    "revision": "9629a9ca3792a81b45e8",
+    "url": "/js/chunk-2d0d6f50.2a849865.js"
+  },
+  {
+    "revision": "a001687924f10429e423",
+    "url": "/js/chunk-2d0f0e03.36ea48f5.js"
+  },
+  {
+    "revision": "f57672eb87b41ec68eeb",
+    "url": "/js/chunk-2d213164.e11e4eba.js"
+  },
+  {
+    "revision": "7d9914c6a30b23f8f72f",
+    "url": "/js/chunk-40fa87f4.015c692c.js"
+  },
+  {
+    "revision": "4ed785de9826d8828191",
+    "url": "/js/chunk-7726dafa.47aa921f.js"
+  },
+  {
+    "revision": "cf7f3f3ac4e408b3d881",
+    "url": "/js/chunk-befbc4f2.308679a4.js"
+  },
+  {
+    "revision": "74604bf4a3fc84913688",
+    "url": "/js/chunk-vendors.18e2dc8b.js"
+  },
+  {
+    "revision": "36879ea23cc528c9d2e27cf1e543dc2c",
+    "url": "/js/index.js"
+  },
+  {
+    "revision": "fe8ef97c7298f7e19389e2f7aa29dd32",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
