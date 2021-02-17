0d531b1c47e348fe1ba93649a58debc8d343d681
Joel Mai <38419872+Inf166@users.noreply.github.com>
2021-02-17 18:49:21 +0100

    [DONE] Add Category to New Recipe

--- VerticalPrototype/src/views/Explore.vue
+++ VerticalPrototype/src/views/Explore.vue
@@ -12,6 +12,7 @@
         <button type="button" class="search-button"><i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!-- Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/></svg></i> Suchen</button>
       </form>
     </div>
+    <!-- TODO Show User filtered received Recipes -->
     <div class="previews">
       <h3>Rezepte meiner Freunde</h3>
       <div class="recipe-teaser" v-for="(recipe, index) in friendRecipies" :key="index">
@@ -24,7 +25,9 @@
           <p><strong>Geschrieben von: </strong><a href="#" target="_blank" v-for="(author, index) in recipe.author" :key="index">{{author}}</a></p>
           <p><strong>Erstellt am: </strong>{{recipe.datePublished}}</p>
         </div>
-        <div class="recipe-teaser-buttons">
+        <!-- TODO Enable Buttons with Functions -->
+        <!-- TODO Show Recipe in Fullpage via router -->
+        <div class="recipe-teaser-buttons"> 
           <button type="button" class="primary-button"><i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 416 512"><!-- Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M207.9 15.2c.8 4.7 16.1 94.5 16.1 128.8 0 52.3-27.8 89.6-68.9 104.6L168 486.7c.7 13.7-10.2 25.3-24 25.3H80c-13.7 0-24.7-11.5-24-25.3l12.9-238.1C27.7 233.6 0 196.2 0 144 0 109.6 15.3 19.9 16.1 15.2 19.3-5.1 61.4-5.4 64 16.3v141.2c1.3 3.4 15.1 3.2 16 0 1.4-25.3 7.9-139.2 8-141.8 3.3-20.8 44.7-20.8 47.9 0 .2 2.7 6.6 116.5 8 141.8.9 3.2 14.8 3.4 16 0V16.3c2.6-21.6 44.8-21.4 48-1.1zm119.2 285.7l-15 185.1c-1.2 14 9.9 26 23.9 26h56c13.3 0 24-10.7 24-24V24c0-13.2-10.7-24-24-24-82.5 0-221.4 178.5-64.9 300.9z"/></svg></i>Jetzt kochen</button>
           <button type="button" class="other-button"><i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!-- Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M0 512V48C0 21.49 21.49 0 48 0h288c26.51 0 48 21.49 48 48v464L192 400 0 512z"/></svg></i>Speichern</button>
           <button type="button" class="secondary-button"><i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"/></svg></i>Verwerfen</button>
@@ -92,6 +95,7 @@ export default {
           console.log("Got Recipies from peer: ");
           console.log(data);
           data.forEach(element => {
+            //TODO Filter through recipes and make user guesses on which Recipe he might like more
             this.$store.dispatch("addFriendRecipies", element);
           });
           console.log(this.friendRecipies);
--- VerticalPrototype/src/views/NewRecipe.vue
+++ VerticalPrototype/src/views/NewRecipe.vue
@@ -7,6 +7,14 @@
         <input type="text" v-model="recipeName" id="rezeptname" @change="toggleColor('rezeptname')" required placeholder="Ein fantastisches Rezept">
         <label for="rezeptbeschreibung">Gebe hier eine kurze Beschreibung des Rezepts an:</label>
         <input type="textarea" v-model="recipeDescription" id="rezeptbeschreibung" @change="toggleColor('rezeptbeschreibung')" required placeholder="Eine kurze Beschreibung">
+        <label for="category">Welcher Kategorie würdest du das Rezept zuordnen? </label>
+        <div class="category" v-for="(category,index) in categories" :key="index">
+          <input type="text" v-model="category.name" placeholder="Vorspeise" id="rezeptkategorie" @change="toggleColor('rezeptkategorie')">
+          <div class="recipe-actions-buttons rm-spacer">
+            <button type="button" class="other-button" @click="removeCategory(index)" v-show="index || ( !index && categories.length > 1)"><i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/></svg></i> Kategorie entfernen</button>
+            <button type="button" class="other-button" @click="addCategory(index)" v-show="index == categories.length-1"><i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/></svg></i> Weitere Kategorien angeben</button>
+          </div>
+        </div>
         <div class="recipe-actions-buttons" v-if="recipeName.length>0">
           <button type="button" class="primary-button" @click="savePartial(recipeHead)"><i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) --><path d="M433.941 129.941l-83.882-83.882A48 48 0 0 0 316.118 32H48C21.49 32 0 53.49 0 80v352c0 26.51 21.49 48 48 48h352c26.51 0 48-21.49 48-48V163.882a48 48 0 0 0-14.059-33.941zM224 416c-35.346 0-64-28.654-64-64 0-35.346 28.654-64 64-64s64 28.654 64 64c0 35.346-28.654 64-64 64zm96-304.52V212c0 6.627-5.373 12-12 12H76c-6.627 0-12-5.373-12-12V108c0-6.627 5.373-12 12-12h228.52c3.183 0 6.235 1.264 8.485 3.515l3.48 3.48A11.996 11.996 0 0 1 320 111.48z"/></svg></i> Speichern</button>
         </div>
@@ -347,6 +355,7 @@ export default {
       recipeName: "",
       recipeDescription: "",
       portion: "",
+      categories: [{name: ''}],
 
       ingredients: [{name:'', amount:'', unit:'', origins: [{name: ''}], alternativeIngredients: [{name: "", amount: "", unit: "", origin: ""}]}],
       utensils: [{name:''}],
@@ -400,6 +409,7 @@ export default {
       recipeHead["description"] = this.recipeDescription;
       recipeHead["datePublished"] = this.now;
       recipeHead["isNew"] = true;
+      recipeHead["categories"] = this.categories;
       var author = {};
       author["name"] = this.myUserName;
       recipeHead["author"] = author;
@@ -445,26 +455,18 @@ export default {
       householdUtensilObj["householdUtensils"] = this.utensils;
       return householdUtensilObj;
     },
-    historicalNoteObj() { //TODO
-      var historicalNoteObj = {};
-      historicalNoteObj["historicalNote"] = [];
-      var temphistoricalNote = {};
-      temphistoricalNote["author"] = this.myUserName;
-      temphistoricalNote["text"] = this.historicalNote;
-      historicalNoteObj["historicalNote"].push(temphistoricalNote);
-      return historicalNoteObj;
-    },
-    tipsAndTricksObj() { //TODO
-      var tipsAndTricksObj = {};
-      tipsAndTricksObj["tipsAndTricks"] = [];
-      var temptipsAndTricks = {};
-      temptipsAndTricks["author"] = this.myUserName;
-      temptipsAndTricks["text"] = this.historicalNote;
-      tipsAndTricksObj["tipsAndTricks"].push(temptipsAndTricks);
-      return tipsAndTricksObj;
-    },
   },
   methods: {
+    addCategory() {
+      this.categories.push({
+        name: '',
+      });
+      console.log(this.categories);
+    },
+    removeCategory(index) {
+      this.categories.splice(index, 1);
+    },
+
     addUtensil() {
       this.utensils.push({
         name: '',
@@ -550,7 +552,7 @@ export default {
         });
         alert("Rezept gespeichert!");
       });
-      console.log(this.$store.getters.myRecipies);
+      console.log(JSON.stringify(this.$store.getters.myRecipies));
     },
     goToStep(prevPage, nextPage) {
       if(prevPage == null && nextPage == null) {
@@ -560,6 +562,32 @@ export default {
             });
           }
       } else {
+        var elementsOnPage = [
+        "rezeptname",
+        "rezeptbeschreibung",
+        "rezeptkategorie",
+        "rezeptportion",
+        "rezeptschwierigkeitsgrad",
+        "rezepthaushaltsgerät",
+        "zutatname",
+        "zutatmenge",
+        "zutateinheit",
+        "zutatherkunft",
+        "altzutatname",
+        "altzutatmenge",
+        "altzutateinheit",
+        "rezeptzubereitungszeit",
+        "rezeptruhezeit",
+        "rezeptkochzeit",
+        "rezeptzubereitungschritt",
+        "rezeptzubereitunganekdote",
+        "rezeptzubereitungtipp",
+      ];
+      var openInput = false;
+      elementsOnPage.forEach(element => {
+        if(document.getElementById(element).classList.contains("edited-but-not-saved"))openInput = true
+      }); 
+      if(!openInput){
         switch (prevPage) {
           case 'showRecipeMeta':
             this.showRecipeMeta = false;
@@ -634,6 +662,9 @@ export default {
           default:
             break;
         }
+      } else {
+        alert("You have unsaved changes... Please save them first.");
+      }
       }
     },
     toggleColor(elementID) {
@@ -643,6 +674,7 @@ export default {
       var elementsOnPage = [
         "rezeptname",
         "rezeptbeschreibung",
+        "rezeptkategorie",
         "rezeptportion",
         "rezeptschwierigkeitsgrad",
         "rezepthaushaltsgerät",
